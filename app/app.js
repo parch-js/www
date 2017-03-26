5e924464f99ad43e2e787236e2be22919b5aeace
Dylan Foster <dylan947@gmail.com>
2017-03-26 15:24:08 -0700

    feat: skeleton

---
+++ app/app.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
+import config from './config/environment';
+
+let App;
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
+App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver,
+
+  engines: {
+    docs: {
+      dependencies: {
+        externalRoutes: {
+          home: "index"
+        }
+      }
+    }
+  }
+});
+
+loadInitializers(App, config.modulePrefix);
+
+export default App;
